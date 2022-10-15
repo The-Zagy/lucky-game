13d7259af85426e52a410aafd1d507c7fbe4b957
ahmedFATHY <af773942@gmail.com>
2022-10-15 21:26:11 +0200

    modifing landing page and crating a js file for it
    creating the functionality of the play locally button, and adding a field to allow the second player to add an ID to join a room with the first player

---
+++ landing.js
@@ -0,0 +1,20 @@
+let btnlocal= document.querySelector(".btnlocal")
+let btnjoin= document.querySelector(".btnjoin")
+let btnsubmit= document.querySelector(".btnsubmit")
+let roomid=document.querySelector(".roomid")
+let  btncreate=document.querySelector(".btncreate")
+///////////////////////////////////////////////////
+function play_local(){
+    location.href="index.html"
+}
+///////////////////////////////////////////////////
+btnjoin.addEventListener('click',function(){
+roomid.classList.remove("hidden")
+btnsubmit.classList.remove("hidden")
+
+})
+btncreate.addEventListener('click',function(){
+
+
+    
+})
